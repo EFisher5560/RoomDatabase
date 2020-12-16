e841f3c8682de85f39a981bd3e48a7b3e25b30d7
EFisher5560 <71767805+EFisher5560@users.noreply.github.com>
2020-12-15 23:06:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.trackmysleepquality.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
